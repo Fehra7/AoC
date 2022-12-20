a3b1052bafcf36e1a343ebd9deb777355443edd1
unknown <rfehra@gmail.com>
2022-12-20 14:09:30 +0100

    first change

--- src/main/kotlin/day01/Main.kt
+++ src/main/kotlin/day01/Main.kt
@@ -1,7 +1,7 @@
 package day01
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
